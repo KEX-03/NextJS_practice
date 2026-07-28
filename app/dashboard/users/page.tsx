3fd895771cd921e48ae9db00aaf9ed744700fb6c
KEX-03 <killex003@outlook.com>
2026-07-28 16:43:21 +0530

    added a list of users in dashboard/users

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,6 +1,15 @@
 const Users = () => {
   return (
-    <div>Users</div>
+    <div>
+      <h1>Dashboard Users</h1>
+
+      <ul className="mt-10">
+        <li>User 1</li>
+        <li>User 2</li>
+        <li>User 3</li>
+        <li>User 4</li>
+      </ul>
+    </div>
   )
 }
 
