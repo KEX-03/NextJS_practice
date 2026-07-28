0030982da3368436960815aa62d6849483244243
KEX-03 <killex003@outlook.com>
2026-07-28 18:04:31 +0530

    added main layout identifier

--- components/hello.tsx
+++ components/hello.tsx
@@ -2,8 +2,6 @@
 
 function Hello() {
 
-  console.log('Client Component')
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="grid text-center lg:max-w-5xl lg:w-full lg:h-full lg:grid-cols-4 lg:text-left">
