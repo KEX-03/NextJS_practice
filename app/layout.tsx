15048465684cf05e39783658308ec0a627c59f3a
KEX-03 <killex003@outlook.com>
2026-07-28 18:44:43 +0530

    linked the home to dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="h-screen flex flex-col overflow-hidden">
         NavBar 
         {children}
         Footer
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,22 @@
-import Hello from '@/components/hello' 
+import Link from "next/link"
 
 function Home() {
 
-  // console.log('Server Component')
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-      </div>
-
-      <div className="flex place-items-center before:absolute before:h-[320px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <div className="grid text-center lg:max-w-5xl lg:w-full lg:h-full lg:grid-cols-4 lg:text-left">
-          <h2 className={`text-5xl font-semibold`}>
+    <main className="flex flex-1 flex-col mt-16 items-center overflow-hidden p-8">
+      <div className="relative flex place-items-center">
+        <div className="absolute inset-0 -z-10 before:absolute before:h-[320px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]" />
+          <div className="relative z-10 grid text-center lg:max-w-5xl lg:w-full lg:grid-cols-4 lg:text-left">
+          <h1 className="text-5xl font-semibold">
             Vivek is the GOAT!!!{' '}
-          </h2>
+          </h1>
+          <h2 className="text-3xl font-semibold mt-8">
+            <Link href="/dashboard">
+              Click Here
+            </Link>
+          </h2>  
         </div>        
       </div>
-      <Hello />
     </main>
   );
 }
